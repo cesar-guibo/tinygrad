Still need to fix numerical stability

aux.py
@@ -1,7 +1,5 @@
 from tinygrad import Tensor
-
-#uniform1 = Tensor.uniform(100, 500, low=0, high=10)
-#uniform2 = Tensor.uniform(500, 100, low=0, high=10)
+from tinygrad.dtype import dtypes
 
 #for item in uniform1.matmul(uniform2).schedule_with_vars()[0]:
 #    print(item)
@@ -13,8 +11,8 @@ from tinygrad import Tensor
 #    print()
 
 a = Tensor([[i for i in range(100)] for _ in range(5000)], device="GPU")
+c = Tensor([i for i in range(50000)], device="GPU")
 b = Tensor([[i] for i in range(50000)], device="GPU")
 
-#print((a + b.expand(a.shape)).numpy())
-#print((a.sum(axis=0)).numpy())
 print((a.cumsum(axis=0)).numpy())
+